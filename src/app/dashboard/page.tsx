0dee9aae4ff8583ad2b734969c2541af1bb33492
frimpong-kofi <noeljoel61@gmail.com>
2023-05-12 14:45:24 +0000

    +: Customized Button, Upstash Redis DB Con, Google Auth

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+interface DashbaordProps {}
+
+const Dashbaord: React.FC<DashbaordProps> = () => {
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
+};
+
+export default Dashbaord;
---
+++ src/lib/auth.ts
@@ -0,0 +1,69 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+
+
+// GET GOOGLE CREDENTIALS FROM LOCAL ENVIRONMENT
+function getGoogleCredentials() {
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
+    if (!clientId || clientId.length === 0) {
+        throw new Error("Missing GOOGLE_CLIENT_ID")
+    }
+    if (!clientSecret || clientSecret.length === 0) {
+        throw new Error("Missing GOOGLE_CLIENT_SECRET")
+    }
+
+    return { clientId, clientSecret }
+}
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: UpstashRedisAdapter(db), // Generate token id for users
+    session: {
+        strategy: 'jwt'
+    },
+    pages: {
+        signIn: '/login'
+    },
+    providers: [
+        GoogleProvider({
+            clientId: getGoogleCredentials().clientId,
+            clientSecret: getGoogleCredentials().clientSecret,
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            const dbUser = (await db.get(`user:${token.id}`)) as User | null  // Check if user is in db with token id
+
+            // If no user / new user, get token id and assign to user
+            if (!dbUser) {
+                token.id = user!.id
+                return token
+            }
+            // If user is not new, return the following
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image,
+            }
+        },
+        // Verify if user has a session
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
+            }
+
+            return session
+        },
+        redirect() {
+            return '/dashboard'
+        }
+    }
+}
